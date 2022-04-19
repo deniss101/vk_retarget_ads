7143c6834ec2302aa9d22ce2a85d67d44e31207c
deniss101 <96088577+deniss101@users.noreply.github.com>
2022-04-19 17:50:44 +0300

    Add main .py file

---
+++ vk_groups_v3.py
@@ -0,0 +1,42 @@
+import requests
+import time
+from progress.bar import IncrementalBar
+
+print("Version 0.3" '\n')
+vk_token = "e536f6226792a4c0c13d9479d6995ff31ba8013cef844f8f851ccdcb42a818686036e25cfa80114cc4683"
+HOST = "https://api.vk.com/method/"
+
+
+def work():
+    target = (input("Введите ID или название сообщества:"))
+    start_time = time.perf_counter()
+    offset = 0
+    people_in_target = requests.get(HOST + 'groups.getMembers', params={'group_id': target, 'access_token': vk_token, 'v': 5.131})
+    people_1000 = people_in_target.json()['response']['count']
+    print("Количество подписчиков:", people_1000)
+
+    result_file = open("ids.txt", "w")
+    bar = IncrementalBar('Выгружаю:', max=people_1000 // 1000 + 1, suffix='%(percent)d%%')
+    for i in range(people_1000 // 1000 + 1):
+        time.sleep(0.3)
+        answer = requests.get(HOST + 'groups.getMembers', params={'group_id': target, 'offset': offset, 'access_token': vk_token, 'v': 5.131})
+        to_write = [*answer.json()['response']['items']]
+        print(to_write)
+        offset += 1000
+        #print(offset // 1000, sep="\n")
+        #for item in to_write:
+            #print(item, file=result_file, sep='\n')
+        bar.next()
+    bar.finish()
+    result_file.close()
+    finish_time = time.perf_counter()
+    print("Успех, ID выгружены.", "Выполнено за", round(finish_time-start_time, 3), "сек." + "\n")
+
+    next = str(input("Получить список из другой группы? " + "\n"))
+    if next == "+":
+        work()
+    else:
+        exit()
+
+
+work()
