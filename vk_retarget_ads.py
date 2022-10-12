0e3651dbfd8132aced360410821136afcd813232
deniss101 <96088577+deniss101@users.noreply.github.com>
2022-10-12 20:43:45 +0300

    Api token in environment variables, prep for PyQt

--- vk_retarget_ads.py
+++ vk_retarget_ads.py
@@ -1,17 +1,18 @@
 import requests
 import time
+from os import environ
 from progress.bar import IncrementalBar
 
-print('config.version' '\n')
-vk_token = ""
+print('Let`s work' '\n')
+vk_token = environ.get('VK_API')
 HOST = "https://api.vk.com/method/"
 
 
 def work():
-    target = (input("Введите ID или название сообщества:"))
+    target = (input("Введите ID или название сообщества: "))
     start_time = time.perf_counter()
     offset = 0
-    people_in_target = requests.get(config.HOST + 'groups.getMembers', params={'group_id': target, 'access_token': config.vk_token, 'v': 5.131})
+    people_in_target = requests.get(HOST + 'groups.getMembers', params={'group_id': target, 'access_token': vk_token, 'v': 5.131})
     people_1000 = people_in_target.json()['response']['count']
     print("Количество подписчиков:", people_1000)
 
@@ -21,22 +22,24 @@ def work():
         time.sleep(0.3)
         answer = requests.get(HOST + 'groups.getMembers', params={'group_id': target, 'offset': offset, 'access_token': vk_token, 'v': 5.131})
         to_write = [*answer.json()['response']['items']]
-        print(to_write)
         offset += 1000
-        #print(offset // 1000, sep="\n")
-        #for item in to_write:
-            #print(item, file=result_file, sep='\n')
+        for item in to_write:
+            print(item, file=result_file, sep='\n')
         bar.next()
     bar.finish()
     result_file.close()
     finish_time = time.perf_counter()
     print("Успех, ID выгружены.", "Выполнено за", round(finish_time-start_time, 3), "сек." + "\n")
 
-    next = str(input("Получить список из другой группы? " + "\n"))
+    next = str(input("Получить список из другой группы? + / - " + "\n"))
     if next == "+":
         work()
     else:
         exit()
+
+
+def test():
+    print('tested')
 
 
 work()
